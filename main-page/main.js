46e94d61d2344923dc935df75668266d71644654
AhmedBasharAli <ahmedba@stud.ntnu.no>
2019-10-18 11:13:30 +0200

    added slideshow

--- main-page/main.js
+++ main-page/main.js
@@ -37,3 +37,121 @@ function createButton(string,id,place){
   place.appendChild(btn);
 }
 
+// Slideshow start //
+let i = 0;
+const images = [];
+let time = 4000;
+const imagetext = [];
+const dots = [];
+
+images[0] = "../img/exampleimg1.jpg";
+images[1] = "../img/exampleimg2.jpg";
+images[2] = "../img/exampleimg3.jpg";
+images[3] = "../img/exampleimg4.jpg";
+
+imagetext[0] = "Velkommen";
+imagetext[1] = "VelkommenVelkommen";
+imagetext[2] = "VelkommenVelkommenVelkommen";
+imagetext[3] = "VelkommenVelkommenVelkommenVelkommen";
+
+dots[0] = "dot1"
+dots[1] = "dot2"
+dots[2] = "dot3"
+dots[3] = "dot4"
+
+
+
+
+function changeImg() {
+  document.slide.src = images[i];
+  document.getElementById("slide_caption").innerHTML = imagetext[i];
+  document.getElementById(dots[i]).style.backgroundColor = "red";
+  if (i >=1) {
+  document.getElementById(dots[i-1]).style.backgroundColor = "white";
+  document.getElementById(dots[i-2]).style.backgroundColor = "white";
+  document.getElementById(dots[i-3]).style.backgroundColor = "white";
+  document.getElementById(dots[i-4]).style.backgroundColor = "white";
+  }
+
+  if(i < images.length -1) {
+    i++;
+  }
+  else {
+    i = 0;
+  }
+  setTimeout("changeImg()", time);
+
+}
+
+window.onload = changeImg;
+
+function nextImage() {
+
+  if (i < images.length-1 && i >= 0) {
+    i++;
+  }
+  else {
+    i=0;
+  }
+  document.slide.src = images[i];
+  document.getElementById("slide_caption").innerHTML = imagetext[i];
+  if (i>=1) {
+  document.getElementById(dots[i-1]).style.backgroundColor = "white";
+  }
+
+  else if (i != 3) {
+    document.getElementById(dots[3]).style.backgroundColor = "white";
+  }
+  document.getElementById(dots[i]).style.backgroundColor = "red";
+}
+
+function prevImage() {
+
+  if(i > 0 && i <= images.length-1) {
+    i--;
+  }
+  else {
+    i=images.length-1
+  }
+  document.slide.src = images[i];
+  document.getElementById("slide_caption").innerHTML = imagetext[i];
+  if (i>=1) {
+    document.getElementById(dots[i+1]).style.backgroundColor = "white";
+    }
+  
+    else if (i != 0) {
+      document.getElementById(dots[0]).style.backgroundColor = "white";
+    }
+    document.getElementById(dots[i]).style.backgroundColor = "red";
+}
+
+function changeDot1() {
+  i = 0;
+  document.slide.src = images[i];
+  document.getElementById("slide_caption").innerHTML = imagetext[i];
+  
+  
+}
+function changeDot2() {
+  i = 1;
+  document.slide.src = images[i];
+  document.getElementById("slide_caption").innerHTML = imagetext[i];
+
+}
+function changeDot3() {
+  i = 2;
+  document.slide.src = images[i];
+  document.getElementById("slide_caption").innerHTML = imagetext[i];
+
+}
+function changeDot4() {
+  i = 3;
+  document.slide.src = images[i];
+  document.getElementById("slide_caption").innerHTML = imagetext[i];
+
+}
+
+
+
+
+//slideshow end //

c8ebd63276f8e32575059551c3b74aced9996e1c
kaffikaffi <borge.dreyer@gmail.com>
2019-10-23 12:31:06 +0200

    add header.js to seperate file

---
+++ main-page/header.js
@@ -0,0 +1,40 @@
+const HEADER_ELEMENT = document.getElementById("header");
+const FOOTER_ELEMENT = document.getElementById("footer");
+const UL_ELEMENT = document.getElementById("nav-ul");
+const MAIN_SECTION = document.getElementById("main");
+const LOGIN_DIV = document.getElementById("login-container");
+const BURGER_MENU = document.getElementById("hamburger-menu");
+
+//Where should global variables be declared? 
+
+let nav_array = [
+  //array can only contain words without spaces
+  "Fitness",
+  "Trainingprogramme",
+  "Supplements",
+  "Calculators"
+];
+
+function renderHeader(ul,array,header){
+  array.forEach(element => {
+    let li = document.createElement("li");
+    li.setAttribute("class", "nav-li");
+    let a = document.createElement("a");
+    a.textContent = element;
+    a.setAttribute("href", element + ".html") //Fix so that it only uses lower-letters
+    li.appendChild(a); //is it even nessecary with a-tags? 
+    ul.appendChild(li); // Fix Styling a-tags
+  });
+  createButton("Log in", "login_btn",LOGIN_DIV);
+  createButton("Register", "register_btn", LOGIN_DIV);
+}
+
+renderHeader(UL_ELEMENT,nav_array)
+
+function createButton(string,id,place){
+  let btn = document.createElement("button");
+  btn.setAttribute("type", "button");
+  btn.setAttribute("id", id);
+  btn.textContent = string;
+  place.appendChild(btn);
+}

75005030bda9855408c89cf638d611b5190df1cd
trygveam <trygveamundsen@live.no>
2019-11-04 20:04:05 +0100

    calorie and rm done

--- calculators/rm/script.js
+++ calculators/rm/script.js
@@ -1,10 +1,21 @@
 const btnCalc = document.getElementById("btn_calculate");
 btnCalc.addEventListener(('click') , () => calculate());
 
+function isValid(input) {
+    return (input > 0 ? true : false)
+}
+
 function calculate(){
     let weight = parseFloat(document.getElementById("input_weight").value);
     let reps = parseFloat(document.getElementById("input_reps").value);
 
-    let oneRm = Math.round(((weight * reps) * 0.0333) + weight);
-    document.getElementById("output_1rm").innerHTML = "Your 1 RM is: " + oneRm;
+    if(isValid(weight) && isValid(reps)){
+        
+        let oneRm = Math.round(((weight * reps) * 0.0333) + weight);
+        document.getElementById("output_1rm").innerHTML = "Your 1 RM is: " + Math.round(oneRm) + " kg";
+    }
+    else{
+        document.getElementById("output_1rm").innerHTML = "Write in weight and reps";
+
+    }
 }

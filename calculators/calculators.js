afcf7ac4cad5a9fa17757d107dfa1496a381bccc
AhmedBasharAli <ahmedba@stud.ntnu.no>
2019-11-05 11:57:40 +0100

    Endret tiles på calculator-page

--- calculators/calculators.js
+++ calculators/calculators.js
@@ -3,25 +3,29 @@ let tile_array = [
       name: "rm",
       display: "1 Rep Max calculator",
       src: "rm/rm.html",
-      img: "img/rm-tile.jpg"
+      img: "../img/gradient.png",
+      icon: "img/rm.png"
     },
     tile2 = {
       name: "maxpulse",
       display:"Find your max pulse",
       src: "maxpulse/maxpulse.html",
-      img: "img/maxpulse-tile.jpeg"
+      img: "../img/gradient.png",
+      icon: "img/maxpulse.png"
     },
     tile3 = {
       name: "calorie",
       display:"Find out your daily calorie needs",
       src: "calorie/calorie.html",
-      img: "img/calorie-tile.jpeg"
+      img: "../img/gradient.png",
+      icon: "img/calorie.png"
     },
     tile4= {
       name: "bmi",
       display:"Find your BMI",
       src: "bmi/bmi.html",
-      img: "img/bmi-tile.jpeg"
+      img: "../img/gradient.png",
+      icon: "img/bmi.png"
     },
     
   ];
@@ -38,13 +42,35 @@ let tile_array = [
         let image = document.createElement("img");
         image.setAttribute("class", "tile-image");
         image.setAttribute("src", element.img);
-        
+
+        let icon = document.createElement("img");
+        icon.setAttribute("class", "tile-icon");
+        icon.setAttribute("src", element.icon);
+
+        let hoverDiv = document.createElement("div");
+        hoverDiv.setAttribute("class", "hover_div");
+        hoverDiv.style.width = "100%";
+        hoverDiv.style.height = "100%";
+        hoverDiv.style.zIndex = "100";
+
         tile.appendChild(image);
+        tile.appendChild(icon);
+        tile.appendChild(hoverDiv);
+
         let display = document.createElement("h3");
         display.setAttribute("class", "tile-header");
         display.textContent = element.display;
         tile.appendChild(display);
         TILE_DIV.appendChild(tile);
+
+        hoverDiv.addEventListener("mouseover", () =>{
+          image.classList.toggle("tile-image-rotate");
+          tile.classList.toggle("tile-style-hover");
+        });
+        hoverDiv.addEventListener("mouseleave", () =>{
+          image.classList.toggle("tile-image-rotate");
+          tile.classList.toggle("tile-style-hover");
+        });
       });
       console.log(tile_array);
     }

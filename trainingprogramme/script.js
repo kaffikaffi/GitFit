4387fe77ae35eeb6fc40246a6db7d5f2b96a4466
Ruben Kobbeltvedt <ruben.kobbeltvedt@gmail.com>
2019-11-05 23:43:04 +0100

    Completed brainfuck function, I hope

--- trainingprogramme/script.js
+++ trainingprogramme/script.js
@@ -2,240 +2,223 @@ const exercises = {
     squat: {
         name: "Squat",
         muscles: ["calves","quadriceps","hamstrings","gluteus","hips","lower_back","abdominals"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     leg_press: {
         name: "Leg press",
         muscles: ["calves","quadriceps","hamstrings","gluteus"],
-        img: "../img/leg_press_img",
-        equipment: "Leg press machine",
-        descreption: "Set cables on lowest setting, pull rope up til torso, slowly return"
+        img: "../img/gradient.png",
+        description: "Set cables on lowest setting, pull rope up til torso, slowly return"
     },
     lunge: {
         name: "Lunge",
         muscles: ["quadriceps","hamstrings","gluteus","hips"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     deadlift:{
         name: "Deadlift",
         muscles: ["calves","quadriceps","hamstrings","gluteus","hips","lower_back","trapezius","abdominals","forearms"],
-        img: "../img/leg_deadlift_img",
-        equipment: "Barbell",
-        descreption: "needs descreption"
+        img: "../img/gradient.png",
+        description: "needs description"
     },
     leg_extention: {
         name: "Leg extention",
         muscles: ["quadriceps"],
-        img: "../img/leg_extention_img",
-        equipment: "leg-extention machine",
-        descreption: "Sit upright and extend legs."
+        img: "../img/gradient.png",
+        description: "Sit upright and extend legs."
     },
     leg_curl: {
         name: "Leg curl",
         muscles: ["calves","hamstrings"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     standing_calf_raise: {
         name: "Standing calf raise",
         muscles: ["calves"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     seated_calf_raise:{
         name: "Seated calf raise",
         muscles: ["calves"],
-        img: "../img/leg_raise_img",
-        equipment: "calves machine",
-        descreption: "Sit upright and extend calves."
+        img: "../img/gradient.png",
+        description: "Sit upright and extend calves."
     },
     hip_adductor: {
         name: "Hip adductor",
         muscles: ["hips"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     bench_press: {
         name: "Bench press",
         muscles: ["pectorals","deltoids","triceps"],
-        img: "../img/bench_press_img",
-        equipment: "bench rack",
-        descreption: "90 degree angle on arms, and push up until fully stretched."
+        img: "../img/gradient.png",
+        description: "90 degree angle on arms, and push up until fully stretched."
     },
     chest_fly: {
         name: "Chest fly",
         muscles: ["pectorals","deltoids"],
-        img: "../img/chest_flies_img",
-        equipment: "cables",
-        descreption: "Cable flies descreption -"
+        img: "../img/gradient.png",
+        description: "Cable flies description -"
     },
     push_up: {
         name: "Push-up",
         muscles: ["abdominals","pectorals","deltoids","triceps"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     pull_down: {
         name: "Pull-down",
         muscles: ["lats","pectorals","deltoids","biceps","forearms"],
-        img: "../img/pull_down_img",
-        equipment: "rope with cables",
-        descreption: "90 degree angle on arms, and pull down until fully stretched."
+        img: "../img/gradient.png",
+        description: "90 degree angle on arms, and pull down until fully stretched."
     },
     pull_up: {
         name: "Pull-up",
         muscles: ["lats","trapezius","pectorals","deltoids","biceps","forearms"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     bent_over_row: {
         name: "Bent-over row",
         muscles: ["lats","trapezius","biceps","forearms"],
-        img: "../img/bent_over_row_img",
-        equipment: "barbell",
-        descreption: "Bend over 90 degrees and lift up barbell with overhand grip, pull bar to upper abs."
+        img: "../img/gradient.png",
+        description: "Bend over 90 degrees and lift up barbell with overhand grip, pull bar to upper abs."
     },
     upright_row: {
         name: "Upright row",
         muscles: ["trapezius","deltoids","biceps","forearms"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     shoulder_press: {
         name: "Shoulder press",
         muscles: ["trapezius","deltoids","triceps"],
-        img: "../img/shoudler_press_img",
-        equipment: "dumbells",
-        descreption: "Stand with feet in shoulder width, and press dumbells over head"
+        img: "../img/gradient.png",
+        description: "Stand with feet in shoulder width, and press dumbells over head"
     },
     shoulder_fly: {
         name: "Shoulder fly",
         muscles: ["trapezius","deltoids","forearms"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     lateral_raise: {
         name: "Lateral raise",
         muscles: ["trapezius","deltoids"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     shoulder_shrug: {
         name: "Shoulder shrug",
         muscles: ["trapezius","deltoids","forearms"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     pushdown: {
         name: "Pushdown",
         muscles: ["triceps"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     triceps_extension: {
         name: "Triceps extension",
         muscles: ["triceps","forearms"],
-        img: "../img/over_head_extention_img",
-        equipment: "rope with cables",
+        img: "../img/gradient.png",
         description: "Rope over head, and push rope forward until fully stretched."
     },
     biceps_curl: {
         name: "Biceps curl",
         muscles: ["biceps","forearms"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     crunch: {
         name: "Crunch",
         muscles: ["abdominals"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     russian_twist: {
         name: "Russian twist",
         muscles: ["abdominals"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     leg_raise: {
         name: "Leg raise",
         muscles: ["hips","abdominals"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     },
     back_extention: {
         name: "Back extention",
         muscles: ["hamstrings","gluteus","lower_back"],
-        img: "../img/squat",
-        equipment: "none",
-        description: "do something"
+        img: "../img/gradient.png",
+        description: "Add description here"
     }
 };
 
-const muscles = [
-    "calves",
-    "quadriceps",
-    "hamstrings",
-    "gluteus",
-    "hips",
-    "lower_back",
-    "lats",
-    "trapezius",
-    "abdominals",
-    "pectorals",
-    "deltoids",
-    "triceps",
-    "biceps",
-    "forearms"
-];
+const muscles = {
+    calves:"Calves",
+    quadriceps:"Quadriceps",
+    hamstrings:"Hamstrings",
+    gluteus:"Gluteus",
+    hips:"Hips",
+    lower_back:"Lower back",
+    lats:"Lats",
+    trapezius:"Trapezius",
+    abdominals:"Abdominals",
+    pectorals:"Pectorals",
+    deltoids:"Deltoids",
+    triceps:"Triceps",
+    biceps:"Biceps",
+    forearms:"Forearms"
+};
 
+//Brainfuck function
 function assignExercises(nrOfDays) {
-    let days = [];
-    let returnArray = [];
+    let days = {};   
+    let availableMuscles = []
+    for(i in muscles) {
+        availableMuscles.push(i);
+    }
+
     for(let i=0; i<nrOfDays; i++) {
-        days.push([]);
-        returnArray.push([])
+        let randomMuscles = [];
+        let nrOfMuscles = Math.floor(availableMuscles.length/(nrOfDays-i));
+        for(let j=0; j<nrOfMuscles; j++) {
+            let randomMuscleIndex = Math.floor(Math.random()*availableMuscles.length)
+            randomMuscles.push(availableMuscles[randomMuscleIndex]);
+            availableMuscles.splice(randomMuscleIndex, 1)
+        }
+        days[i+1] = {}
+        days[i+1].muscles = randomMuscles
     }
 
-    let i = 0;
-    while(muscles.length > 0) {
-        let randomMuscle = muscles[Math.floor(Math.random()*muscles.length)];
-        days[i%nrOfDays].push(randomMuscle);
-        muscles.splice(muscles.indexOf(randomMuscle), 1);
-        i++;
-    }
+    for(day in days) {
+        let randomExercises = []
 
-    for(let day of days) {
-        for(let exercise in exercises) {
-            for(let muscle of exercises[exercise]) {
-                if(day.includes(muscle)) {
-                    returnArray[days.indexOf(day)].push(String(exercise));
+        for(muscle of days[day]["muscles"]) {
+            for(exercise in exercises) {
+                if(exercises[exercise]["muscles"].includes(muscle)) {
+                    randomExercises.push(exercise)
                 }
             }
         }
+        while(new Set(randomExercises).size > 10) {
+            randomExercises.splice(Math.floor(Math.random()*randomExercises.length), 1)
+            console.log(new Set(randomExercises).size)
+        }
+        days[day].exercises = Array.from(new Set(randomExercises))
     }
-    return returnArray;
 }
 
 let nrOfDays = document.getElementById("days").value;
 let btnGenerate = document.getElementById("generate");
 
-btnGenerate.addEventListener("input", assignExercises(nrOfDays))
+btnGenerate.addEventListener("click", assignExercises(nrOfDays))

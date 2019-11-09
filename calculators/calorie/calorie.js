e7e309b92ba8c2621dd0aa31339ded8ceaf2af1b
trygveam <trygveamundsen@live.no>
2019-11-09 19:39:10 +0100

    added comments and final cleanup

--- calculators/calorie/calorie.js
+++ calculators/calorie/calorie.js
@@ -1,17 +1,20 @@
 const btnCalculate = document.getElementById("btn_calculate");
 btnCalculate.addEventListener(('click') , () => calculateCalories());
  
-
+// Function that checks if the input value is higher than 0
 function isValid(input) {
      return (input > 0 ? true : false)
  }
 
+
+// Function that calculates the calorie intake and writes it in html, gives a different amount of calories depending on the inputs. 
 function calculateCalories(){
 
     let weight = parseFloat(document.getElementById("input_weight").value);
     let height = parseFloat(document.getElementById("input_height").value);
     let age = parseFloat(document.getElementById("input_age").value);
 
+    //checks if all the values in the inputfield are correct and return true
     if(isValid(weight) && isValid(height) && isValid(age) && document.querySelector('input[type=radio]:checked').checked === true){
      let gender = document.querySelector('input[name="gender"]:checked').value;
     let selectElement = document.getElementById("input_activity");
@@ -64,13 +67,9 @@ else{
      document.getElementById("maintain").innerHTML = "";
      document.getElementById("gain_05").innerHTML = "";
      document.getElementById("gain_1").innerHTML = "";
-
-
-
-
 }
 }
-
+// If enterkey is presset, the function calculate will begin
 document.body.onkeyup = function(e) {       
     if (e.keyCode == 13) {
         calculateCalories();
